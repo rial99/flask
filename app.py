66d0bcf28c8940dbe11029a2b470f85224cda29e
Rial Hammer <ratulstephenite99@gmail.com>
2017-12-25 12:24:45 -0800

    work on navigation bar links {contact_us}

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask,render_template
 
 app = Flask(__name__)
+var = [{'name':'Ratul','job':'programmer'},{'name':'Diva','job':'financer'}]
+
 
 @app.route('/')
 def index():
     return render_template('home.html')
+
+@app.route('/Contact_us')
+def Contact():
+    return render_template('contact.html',dev=var)
+
+@app.route('/about/<string:job>/')
+def about(job):
+    return render_template('about.html',jav=job)
 
 if __name__ == '__main__':
     app.run(debug=True,port=80)
